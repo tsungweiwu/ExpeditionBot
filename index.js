b08d3ef2f3c6197f4b08699fce40448a5ffb61f0
Tsung Wei Wu <tsungweiwu@gmail.com>
2021-02-02 02:09:25 -0500

    reminder

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ client.on('ready', () => {
     // client.channels.cache.get('783513086429888515').send("hi")
 });
 
-
 function message() {
     client.channels.cache.get('783513086429888515').send("@here It's time for expeds, Please react to when you can show up <:MMDino:" + client.emojis.cache.find(emoji => emoji.name === "MMDino") + ">").then(function(message) {
         message.react(client.emojis.cache.find(emoji => emoji.name === "OnTime")).then(
@@ -50,7 +49,7 @@ function message() {
             message.react(client.emojis.cache.find(emoji => emoji.name === "302"))).then(
             message.react(client.emojis.cache.find(emoji => emoji.name === "452"))).then(
             message.react(client.emojis.cache.find(emoji => emoji.name === "Skip"))).then(
-                setTimeout(() => {
+                schedule.scheduleJob('r-job', '59 * * * *', function() {
                     var find_max = {
                         'onTime': message.cache.get(client.emojis.cache.find(emoji => emoji.name === "OnTime")).count,
                         'fifteen': message.cache.get(client.emojis.cache.find(emoji => emoji.name === "152")).count,
@@ -58,7 +57,6 @@ function message() {
                         'fourtyf': message.cache.get(client.emojis.cache.find(emoji => emoji.name === "452")).count,
                         'skip': message.cache.get(client.emojis.cache.find(emoji => emoji.name === "Skip")).count
                     }
-
                     var max;
                     for (var h in find_max) {
                         if (!max || (find_max[h] > find_max[max])) {
@@ -68,22 +66,34 @@ function message() {
 
                     switch (max) {
                         case 'onTime': {
-                            alertExpeds(0);
+                            schedule.scheduleJob('alert-job', '0 * * * *', function() {
+                                alertExpeds()
+                                schedule.cancelJob()
+                            })
                             break;
                         }
 
                         case 'fifteen': {
-                            alertExpeds(60000*15);
+                            schedule.scheduleJob('alert-job', '15 * * * *', function() {
+                                alertExpeds()
+                                schedule.cancelJob()
+                            })
                             break;
                         }
 
                         case 'thirty': {
-                            alertExpeds(60000*30);
+                            schedule.scheduleJob('alert-job', '30 * * * *', function() {
+                                alertExpeds()
+                                schedule.cancelJob()
+                            })
                             break;
                         }
 
                         case 'fourtyf': {
-                            alertExpeds(60000*45);
+                            schedule.scheduleJob('alert-job', '45 * * * *', function() {
+                                alertExpeds()
+                                schedule.cancelJob()
+                            })
                             break;
                         }
 
@@ -91,14 +101,17 @@ function message() {
                             break;
                         }
                     }
-                }, 600000)
+
+                    schedule.cancelJob('m-job')
+                })
             )
     }).catch(function() {
         console.log("failed")
     })
 }
 
-function alertExpeds(delayTime) {
-    setTimeout(client.channels.cache.get('783513086429888515').send("@here Please log on now! <:MMDino:" + client.emojis.cache.find(emoji => emoji.name === "MMDino") + ">"), delayTime)
+
+function alertExpeds() {
+    client.channels.cache.get('783513086429888515').send("@here Please log on now! <:MMDino:" + client.emojis.cache.find(emoji => emoji.name === "MMDino") + ">")
 }
 
