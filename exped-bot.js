bb03252deddfb68c4be6fe159c771480a0114ebd
Tsung Wei Wu <tsungweiwu@gmail.com>
2021-03-28 03:18:20 -0400

    bosses added

--- exped-bot.js
+++ exped-bot.js
@@ -59,7 +59,155 @@ client.on('ready', () => {
 });
 
 let infoMap = new Map([
-    ['.buffs', 'https://media.discordapp.net/attachments/790451126956064778/796177528129060894/unknown.png']
+    ['.buffs', {'type' : 'image', 'img' : 'https://media.discordapp.net/attachments/790451126956064778/796177528129060894/unknown.png'}],
+    ['.hilla', {'type' : 'embed', embed: {
+            image: {
+                url: 'https://nisrockk.files.wordpress.com/2011/06/mob_hilla.png'
+            },
+            color: 15406156,
+            title: "Hilla (Commander Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=3039ovmaIk4)",
+            footer: {
+                text: '═════ Boss Info (Party) ═════\n' +
+                    'Normal \t HP: 24.5 b | Crit Resist: 30%\n' +
+                    'Hard \t HP: 31.5 b | Crit Resist: 32%\n' +
+                    'Chaos \t HP: 90.0 b | Crit Resist: 34%\n\n' +
+                    '═════ Boss Info (Single) ═════\n' +
+                    'Normal \t HP: 7.0 b | Crit Resist: 31%\n' +
+                    'Hard \t HP: 9.0 b | Crit Resist: 33%\n' +
+                    'Chaos \t HP: 12.0 b | Crit Resist: 35%\n'
+            }
+        }}],
+    ['.vonleon', {'type' : 'embed', embed: {
+            image: {
+                url: 'http://media.playpark.net/MapleStory/uploadimages/von_leon.gif'
+            },
+            color: 15406156,
+            title: "Von Leon (Commander Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=VlMFDqNf0cs)",
+            footer: {
+                text: '═════ Boss Info (Party) ═════\n' +
+                    'Normal \t HP: 2.45 b | Crit Resist: 22%\n' +
+                    'Hard \t HP: 5.6 b | Crit Resist: 26%\n' +
+                    'Chaos \t HP: 45.0 b | Crit Resist: 30%\n\n' +
+                    '═════ Boss Info (Single) ═════\n' +
+                    'Normal \t HP: 0.595 b | Crit Resist: 20%\n' +
+                    'Hard \t HP: 1.6 b | Crit Resist: 24%\n' +
+                    'Chaos \t HP: 6.0 b | Crit Resist: 28%\n'
+            }
+        }}],
+    ['.vellum', {'type' : 'embed', embed: {
+            image: {
+                url: 'https://rebootms.files.wordpress.com/2016/05/belum.png?w=257&h=300'
+            },
+            color: 15406156,
+            title: "Vellum (Root Abyss Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=3A0FIVEshVU)",
+            footer: {
+                text: '═════ Boss Info ═════\n' +
+                    'Normal \t HP: 34.0 b | Crit Resist: 30%\n' +
+                    'Hard \t HP: 100.0 b | Crit Resist: 32%\n' +
+                    'Chaos \t HP: 170.0 b | Crit Resist: 35%\n'
+            }
+        }}],
+    ['.queen', {'type' : 'embed', embed: {
+            image: {
+                url: 'https://rebootms.files.wordpress.com/2016/05/bloody_queen1.gif?w=180&h=300'
+            },
+            color: 15406156,
+            title: "Crimson Queen (Root Abyss Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=cT1PYgl6CSA)",
+            footer: {
+                text: '═════ Boss Info ═════\n' +
+                    'Normal \t HP: 38.5 b | Crit Resist: 30%\n' +
+                    'Hard \t HP: 110.0 b | Crit Resist: 32%\n' +
+                    'Chaos \t HP: 180.0 b | Crit Resist: 35%\n'
+            }
+        }}],
+    ['.pierre', {'type' : 'embed', embed: {
+            image: {
+                url: 'https://rebootms.files.wordpress.com/2016/05/pierre1.gif?w=620'
+            },
+            color: 15406156,
+            title: "Pierre (Root Abyss Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=tHpgCJGlMcY)",
+            footer: {
+                text: '═════ Boss Info ═════\n' +
+                    'Normal \t HP: 25.2288 b | Crit Resist: 30%\n' +
+                    'Hard \t HP: 69.12 b | Crit Resist: 32%\n' +
+                    'Chaos \t HP: 110.0 b | Crit Resist: 35%\n'
+            }
+        }}],
+    ['.vonbon', {'type' : 'embed', embed: {
+            image: {
+                url: 'https://rebootms.files.wordpress.com/2016/05/banban1.gif?w=620'
+            },
+            color: 15406156,
+            title: "Von Bon (Root Abyss Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=oqcvehH6IWY)",
+            footer: {
+                text: '═════ Boss Info ═════\n' +
+                    'Normal \t HP: 25.2288 b | Crit Resist: 30%\n' +
+                    'Hard \t HP: 69.12 b | Crit Resist: 32%\n' +
+                    'Chaos \t HP: 110.0 b | Crit Resist: 35%\n'
+            }
+        }}],
+    ['.cygnus', {'type' : 'embed', embed: {
+            image: {
+                url: 'https://spadow.files.wordpress.com/2010/12/cygnus-die1.gif?w=630'
+            },
+            color: 15406156,
+            title: "Cygnus Empress (Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=2h2ISyWRNe8)",
+            footer: {
+                text: '═════ Boss Info ═════\n' +
+                    'Normal \t HP: 0.555 b | Crit Resist: 0%\n' +
+                    'Hard \t HP: 4.344 b | Crit Resist: 0%\n' +
+                    'Chaos \t HP: 21.0 b | Crit Resist: 30%\n'
+            }
+        }}],
+    ['.pinkbean', {'type' : 'embed', embed: {
+            image: {
+                url: 'https://media3.giphy.com/media/dqqKpkphmoRrmkvN7Y/giphy.gif'
+            },
+            color: 15406156,
+            title: "Pink Bean (Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=cRpRmg6SlZw)",
+            footer: {
+                text: '═════ Boss Info ═════\n' +
+                    'Normal \t HP: 0.504 b | Crit Resist: 0%\n' +
+                    'Hard \t HP: 2.43 b | Crit Resist: 0%\n' +
+                    'Chaos \t HP: 15.0 b | Crit Resist: 20%\n'
+            }
+        }}],
+    ['.horntail', {'type' : 'embed', embed: {
+            image: {
+                url: 'http://media.playpark.net/MapleStory/uploadimages/horntail.gif'
+            },
+            color: 15406156,
+            title: "Horn Tail (Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=Sk1nHJtPDzY)",
+            footer: {
+                text: '═════ Boss Info ═════\n' +
+                    'Normal \t HP: 0.141 b | Crit Resist: 0%\n' +
+                    'Hard \t HP: 1.573 b | Crit Resist: 0%\n' +
+                    'Chaos \t HP: 12.412 b | Crit Resist: 10%\n'
+            }
+        }}],
+    ['.zakum', {'type' : 'embed', embed: {
+            image: {
+                url: 'http://media.playpark.net/MapleStory/uploadimages/zakum.gif'
+            },
+            color: 15406156,
+            title: "Zakum (Boss)",
+            description: "[Video Guide](https://www.youtube.com/watch?v=zKFZcfPdEr4)",
+            footer: {
+                text: '═════ Boss Info ═════\n' +
+                    'Normal \t HP: 0.070 b | Crit Resist: 0%\n' +
+                    'Hard \t HP: 0.264 b | Crit Resist: 0%\n' +
+                    'Chaos \t HP: 3.529 b | Crit Resist: 5%\n'
+            }
+        }}]
 ]);
 
 client.on('message', message => {
@@ -126,9 +274,18 @@ client.on('message', message => {
     }
 
     if (infoMap.has(message.content)) {
-        let attachment = new Discord.MessageAttachment(infoMap.get(message.content));
+        let item = infoMap.get(message.content);
+        let output;
+
+        if (item.type === 'image') {
+            output = new Discord.MessageAttachment(item.img);
+        }
+        else if (item.type === 'embed') {
+            output = {embed: item.embed};
+        }
+
         try {
-            return message.channel.send(attachment);
+            return message.channel.send(output);
         } catch (err) {
             console.error(err);
         }
