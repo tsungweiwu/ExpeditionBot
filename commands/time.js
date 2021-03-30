259dbababe7f3047f3325545ec4a2034ffe6c771
Tsung Wei Wu <tsungweiwu@gmail.com>
2021-03-30 17:00:40 -0400

    time

---
+++ commands/time.js
@@ -0,0 +1,17 @@
+module.exports = {
+    category: 'Guides',
+    callback: ({ message }) => {
+        const targetChannel = message.channel;
+
+        let serverTime = new Date(new Date().toLocaleString("en-US", {timeZone: "Etc/GMT+8"}));
+        // hours as (HH) format
+        let hours = ("0" + serverTime.getHours()).slice(-2);
+        // minutes as (mm) format
+        let minutes = ("0" + serverTime.getMinutes()).slice(-2);
+
+        targetChannel.send("It is now " + hours + ":" + minutes + " server time");
+    },
+    error: ({ error, command, message, info }) => {
+        message.channel.send("Incorrect input!")
+    }
+}
