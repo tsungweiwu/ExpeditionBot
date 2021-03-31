da4b8c95d1d55d9c33a76a95c5b0f4e2fc4abce7
Tsung Wei Wu <tsungweiwu@gmail.com>
2021-03-31 02:54:22 -0400

    gm role

--- commands/configure.js
+++ commands/configure.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 module.exports = {
     category: 'Signup',
     callback: ({ message, client }) => {
-        if (message.member.roles.cache.some(role => role.name !== 'GM')) return message.reply("You need to have the 'GM' role to access this command");
+        if (!message.member.roles.cache.some(role => role.name === 'GM')) return message.reply("You need to have the 'GM' role to access this command");
 
         let str = message.content.split(" ");
         if (str.length < 2) return message.reply('List of configurable options', {
