50cd0b6c08f26c8cb29c9443ec06e47e597315f9
Edison <tsungweiwu@gmail.com>
2021-04-13 18:12:58 +0000

    ra ch

--- features/bossRunAnnouncement.js
+++ features/bossRunAnnouncement.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 module.exports = {
     message: function (title, description, hour, minute, client, config) {
-        if (config.channelId === "") return;
+        if (config.raChannelId === "") return;
         if (client.channels.cache.get(config.raChannelId) === undefined) return;
 
         const titleName = {'✅': 'Attending', '❌': 'Not Attending', '❓': 'Tentative'};
