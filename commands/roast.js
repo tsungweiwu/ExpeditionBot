31a30c89a46bab702323a4141f9be98c6060ae3d
Tsung Wei Wu <tsungweiwu@gmail.com>
2021-06-06 01:11:33 -0400

    edi

--- commands/roast.js
+++ commands/roast.js
@@ -9,6 +9,10 @@ module.exports = {
             url: "https://insult.mattbas.org/api/insult",
             json: true
         }, (err, response, body) => {
+            if (message.content.includes('<@!156232419219996672>')) {
+                return channel.send('I shall not disrespect my creator <3')
+            }
+
             if (!message.author.bot) {
                 // The author of the last message wasn't a bot
                 return channel.send(body);
