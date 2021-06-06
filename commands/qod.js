a5bb4128d64b6cdd0c9e89928b26f253a0f4822e
Tsung Wei Wu <tsungweiwu@gmail.com>
2021-06-06 12:39:32 -0400

    quote of day

--- commands/qod.js
+++ commands/qod.js
@@ -6,16 +6,25 @@ module.exports = {
         let channel = message.channel; // <-- your pre-filled channel variable
 
         request({
-            url: "https://insult.mattbas.org/api/insult",
+            url: "https://quotes.rest/qod.json?category=inspire",
             json: true
         }, (err, response, body) => {
             if (message.content.includes('156232419219996672')) {
                 return channel.send('I shall not disrespect my creator <3')
             }
-            
+
             if (!message.author.bot) {
                 // The author of the last message wasn't a bot
-                return channel.send(body);
+                return channel.send({
+                    embed: {
+                        footer: {
+                            text: "- " + body.contents.quotes[0].author + ", " + body.contents.quotes[0].date
+                        },
+                        title: body.contents.quotes[0].title,
+                        description: body.contents.quotes[0].quote,
+                        color: '#FFA500'
+                    }
+                });
             }
         });
     }
