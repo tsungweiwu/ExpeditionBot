8935c73fb86e4eb78869254c477d016222d07228
Tsung Wei Wu <tsungweiwu@gmail.com>
2021-06-29 13:18:33 -0400

    conv

---
+++ commands/conv.js
@@ -0,0 +1,19 @@
+module.exports = {
+    category: 'Misc',
+    callback: ({ message }) => {
+        let str = message.content.split(" ");
+        let deg = str[1];
+        let temperature = Number(str[2]);
+        if (!isNaN(temperature)) {
+            if (deg === 'f') {
+                let convertedTemp = (temperature * 9/5) + 32;
+                return message.channel.send(temperature + ' c = ' + convertedTemp + ' f');
+            }
+            else if (deg === 'c') {
+                let convertedTemp = (temperature - 32) * 5/9;
+                return message.channel.send(temperature + ' f = ' + convertedTemp + ' c');
+            }
+            else return message.channel.send('wrong input');
+        }
+    }
+}
