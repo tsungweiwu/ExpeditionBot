8a0c65a2bc613e2f873beebb68b502f736aa5207
Tsung Wei Wu <tsungweiwu@gmail.com>
2021-07-30 20:32:04 -0400

    av

--- commands/avatar.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    category: 'Misc',
-    callback: ({ message }) => {
-        const user = message.mentions.users.first() || message.author;
-        message.channel.send(`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=256`)
-    }
-}
