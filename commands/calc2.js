dcf77136a5b0a1c819d61cccfc8f78f7b2e99f33
Tsung Wei Wu <tsungweiwu@gmail.com>
2021-09-13 13:20:49 -0600

    asia time

---
+++ commands/calc2.js
@@ -0,0 +1,38 @@
+module.exports = {
+    category: 'Guides',
+    callback: ({ message }) => {
+        const content = message.content;
+        const targetChannel = message.channel;
+        const author = message.author;
+
+        let str = content.split(" ");
+
+        let serverTime = new Date(new Date().toLocaleString("en-US", {timeZone: "Asia/Shanghai"}));
+        // hours as (HH) format
+        let hours = ("0" + serverTime.getHours()).slice(-2);
+        // minutes as (mm) format
+        let minutes = ("0" + serverTime.getMinutes()).slice(-2);
+
+        let timeStr = str[1].split(":");
+        let inputHour = Number(timeStr[0]);
+        let inputMin = Number(timeStr[1]);
+
+        if (inputHour > 23 || inputHour < 0 || inputMin < 0 || inputMin > 60) return message.reply("Invalid Input");
+
+        let timeStart = Number(hours*60) + Number(minutes);
+        let timeEnd = inputHour*60 + inputMin;
+
+        if (isNaN(timeEnd)) return message.reply("Invalid Input");
+
+        let difference = timeEnd - timeStart;
+
+        if (difference < 0) {
+            difference = 24*60 + difference;
+        }
+
+        message.reply("You will need to load " + difference + " minutes of AB");
+    },
+    error: ({ error, command, message, info }) => {
+        message.channel.send("Incorrect input! Use .calc 00:00")
+    }
+}
